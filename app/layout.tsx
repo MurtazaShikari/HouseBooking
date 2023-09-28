a457e69c9793be51fe7d15ad368a689fc0f0e5f1
MurtazaShikari <114511800+MurtazaShikari@users.noreply.github.com>
2023-09-28 22:04:42 +0530

    added logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import { Nunito } from "next/font/google";
-
-// const inter = Inter({ subsets: ["latin"] });
+import Navbar from "./components/navbar/Navbar";
 
 export const metadata: Metadata = {
   title: "Airbnb",
