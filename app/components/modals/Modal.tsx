ede31f020be884d5129484f82374de0c163d7f3c
MurtazaShikari <114511800+MurtazaShikari@users.noreply.github.com>
2023-09-29 12:22:44 +0530

    added register modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -64,18 +64,20 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
         <div
-          className="relative w-full
-        md:w-4/6
-        lg:w-3/6
-        xl:w-2/5
-        my-6
-        mx-auto
-        h-full
-        lg:h-auto
-        md:h-auto
-        "
+          className="
+          relative 
+          w-full
+          md:w-4/6
+          lg:w-3/6
+          xl:w-2/5
+          my-6
+          mx-auto
+          h-full
+          lg:h-auto
+          md:h-auto
+          "
         >
           <div
             className={`translate duration-300 h-full ${
@@ -94,7 +96,7 @@ const Modal: React.FC<ModalProps> = ({
             </div>
             <div className="relative p-6 flex-auto">{body}</div>
             <div className="flex flex-col gap-2 p-6">
-              <div className="flex flex-row items-center gap-4 w-dull ">
+              <div className="flex flex-row items-center gap-4 w-full ">
                 {secondaryAction && secondaryActionLabel && (
                   <Button
                     disabled={disabled}
@@ -109,6 +111,7 @@ const Modal: React.FC<ModalProps> = ({
                   onClick={handleSubmit}
                 />
               </div>
+              {footer}
             </div>
           </div>
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import RegisterModal from "./components/modals/RegisterModal";
 import ClientOnly from "./components/ClientOnly";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <Navbar />
           {children}
