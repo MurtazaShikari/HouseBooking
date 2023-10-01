fd3c252679c24e5312ee131989806ef12ba844eb
MurtazaShikari <114511800+MurtazaShikari@users.noreply.github.com>
2023-10-01 11:12:07 +0530

    added map for location step

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -16,17 +16,24 @@ L.Icon.Default.mergeOptions({
 });
 
 interface MapProps {
-  center?: number;
+  center?: number[];
 }
 
 const Map: React.FC<MapProps> = ({ center }) => {
+  console.log(center);
   return (
     <MapContainer
       center={(center as L.LatLngExpression) || [51, -0.09]}
       zoom={center ? 4 : 2}
       scrollWheelZoom={false}
       className="h-[35vh] rounded-lg"
-    ></MapContainer>
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {center && <Marker position={center as L.LatLngExpression} />}
+    </MapContainer>
   );
 };
 
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,8 @@ import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import Map from "../Map";
+import dynamic from "next/dynamic";
+// import { latLng } from "leaflet";
 
 enum STEPS {
   CATEGORY = 0,
@@ -46,6 +48,14 @@ const RentModal = () => {
 
   const category = watch("category");
   const location = watch("location");
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -118,7 +128,7 @@ const RentModal = () => {
           value={location}
           onChange={(value) => setCustomValue("location", value)}
         />
-        <Map />
+        <Map center={location?.latlng} />
       </div>
     );
   }
