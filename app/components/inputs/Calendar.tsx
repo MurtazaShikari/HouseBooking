9837b4034a523229977774c0acf80033e72b0937
MurtazaShikari <114511800+MurtazaShikari@users.noreply.github.com>
2023-10-02 12:09:45 +0530

    added calendar

---
+++ app/components/inputs/Calendar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import { DateRange, Range } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
+interface CalendarProps {
+  value: Range;
+  disabledDates?: Date[];
+  onChange: (value: Range) => void;
+}
+
+const Calendar: React.FC<CalendarProps> = ({
+  value,
+  onChange,
+  disabledDates,
+}) => {
+  return (
+    <DateRange
+      rangeColors={["#262626"]}
+      ranges={[value]}
+      date={new Date()}
+      onChange={onchange}
+      direction="vertical"
+      showDateDisplay={false}
+      minDate={new Date()}
+      disabledDates={disabledDates}
+    />
+  );
+};
+
+export default Calendar;
---
+++ app/components/listings/ListingReservation.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Range } from "react-date-range";
+import Calendar from "../inputs/Calendar";
+interface ListingReservationProps {
+  price: number;
+  totalPrice: number;
+  dateRange: Range;
+  onChange: (value: Range) => void;
+  onSubmit: () => void;
+  disabled?: boolean;
+  disabledDates: Date[];
+}
+
+const ListingReservation: React.FC<ListingReservationProps> = ({
+  price,
+  totalPrice,
+  dateRange,
+  onChange,
+  onSubmit,
+  disabled,
+  disabledDates,
+}) => {
+  return (
+    <div
+      className="
+        bg-white
+        rounded-xl
+        border-[1px]
+        border-neutral-200
+        overflow-hidden
+        "
+    >
+      <div className="flex flex-row items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">${price}</div>
+        <div className="font-light text-neutral-600">night</div>
+      </div>
+      <hr />
+      <Calendar
+        value={dateRange}
+        disabledDates={disabledDates}
+        onChange={(value) => onChange(value.selection)}
+      />
+    </div>
+  );
+};
+
+export default ListingReservation;
